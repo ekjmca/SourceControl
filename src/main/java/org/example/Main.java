54e935368c5588f327f77922896cf277edd0929a
Jordan <jordan_westbury@hotmail.com>
2022-11-10 14:35:27 +0000

    config files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        int ageJ=33;
 
         System.out.println("Wotcher!");
         System.out.println("Yer a Wizard 'Arry");
         System.out.println("Iiii'm a what??!");
-        System.out.println("But Hagrid, there must be a  mistake, this says Platform 9&three quarters, there's no such thing, is there?");
+        System.out.println("But Hagrid, there must be a mistake, this says Platform 9&three quarters, there's no such thing, is there?");
+        System.out.println("Jordan is: "+ ageJ);
     }
 }
